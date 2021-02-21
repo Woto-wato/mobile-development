a1ef228a3598649fbb653139d6dce310ac42210b
wotowato <watowoto@gmail.com>
2021-02-21 10:12:30 +0300

    MD

--- src/hello.kt
+++ src/hello.kt
@@ -1,4 +1,10 @@
 fun main() {
-    Val name;String="woto"
-     val age:Int=19
-}   print[name]
+    var student=arrayOf("watoDida","0799548541",38.675438,20,"true")
+    var name="watoDida"
+    var age =20
+    var phoneNumber="0799548541"
+    var citizen="true"
+    var weight=38.675438
+    print(student)
+
+}
